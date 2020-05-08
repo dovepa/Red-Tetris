6f29daca0271e66efa0769a8931382387096ece5
Dove Pa <dove@frenchgorilla.com>
2020-05-08 15:36:30 +0200

    starting red tetris

--- src/serveur/dist/controllers/chat.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMessage = (aaa) => {
-    return aaa;
-};
-exports.newMessage = (aaa) => {
-    return aaa;
-};
--- src/serveur/dist/util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let DEBUG = true;
-let log = (...args) => {
-    if (DEBUG === true)
-        console.log("[[ Red-Tetris API ]] :: ", args);
-};
-exports.log = log;
